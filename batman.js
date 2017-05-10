11cd8876d6f1b1c81373820dac9f0ead88edae81
Daniela Ortiz <danielaorc06@gmail.com>
2017-05-09 22:11:55 -0400

    added batman slide story

---
+++ batman.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+  //configuration
+  var width = 430;
+  var animationSpeed = 1000;
+  var pause = 3000;
+  var currentSlide = 1;
+
+  //cache DOM
+
+  var $slider = $('#slider');
+  var $slideContainer = $slider.find('.slides');
+  var $slides = $slideContainer.find('.slide');
+
+  var interval;
+
+  function startSlider() {
+
+   interval = setInterval(function () {
+    $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function(){
+      currentSlide++;
+      if (currentSlide == $slides.length) {
+          currentSlide = 1;
+          $slideContainer.css('margin-left', 0);
+      }
+  });
+  }, pause);
+
+}
+
+  function pauseSlider(){
+      clearInterval(interval);
+  }
+
+    $slider.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
+
+    startSlider();
+
+
+});
