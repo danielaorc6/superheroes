ee54896a13fc08e1a150684c19dc9b81d81717e2
Daniela Ortiz <danielaorc06@gmail.com>
2017-05-10 20:59:32 -0400

    adding superman story

--- batman.js
+++ batman.js
@@ -37,4 +37,36 @@ $(document).ready(function(){
     startSlider();
 
 
+    // --------------------SUPERMAN-------
+
+      var currentSlide2 = 1;
+
+    var $slider2 = $('#slider2');
+    var $slideContainer2 = $slider2.find('.slides2');
+    var $slides2 = $slideContainer2.find('.slide2');
+
+    var interval2;
+
+    function startSlider2() {
+
+     interval2 = setInterval(function () {
+      $slideContainer2.animate({'margin-left': '-='+width}, animationSpeed, function(){
+        currentSlide2++;
+        if (currentSlide2 == $slides2.length) {
+            currentSlide2 = 1;
+            $slideContainer2.css('margin-left', 0);
+        }
+    });
+    }, pause);
+
+  }
+
+    function pauseSlider2(){
+        clearInterval(interval);
+    }
+
+      $slider2.on('mouseenter', pauseSlider2).on('mouseleave', startSlider2);
+
+      startSlider2();
+
 });
